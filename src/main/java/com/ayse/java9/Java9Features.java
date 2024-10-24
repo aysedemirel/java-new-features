0df4d654d59b83e09037e87cf49e087221cfe3cf
Ayşe Demirel Deniz <aysedemirel1996@gmail.com>
2024-10-25 02:21:03 +0900

    Add examples for Java 8 and 9 features

---
+++ src/main/java/com/ayse/java9/Java9Features.java
@@ -0,0 +1,31 @@
+package com.ayse.java9;
+
+import java.util.List;
+
+/*
+ * Java 9 Features
+ *
+ * @author aysedemirel
+ * */
+interface Card {
+
+    private static void displayCardDetails() {
+        // Method implementation goes here.
+        System.out.println("displayCardDetails");
+    }
+
+    private Long createCardID() {
+        // Method implementation goes here.
+        System.out.println("createCardID");
+        return 0L;
+    }
+}
+
+public class Java9Features {
+
+    public static void main(String[] args) {
+        List<String> immutableList = List.of();
+        List<String> immutableListWithValue = List.of("one", "two", "three");
+    }
+
+}
